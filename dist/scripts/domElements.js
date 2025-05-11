5a7f05c52d80283e3383b261ca4295f1e4ec31e5
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-05-11 09:40:51 -0300

    feat: create input to enter the new task

--- dist/scripts/domElements.js
+++ dist/scripts/domElements.js
@@ -1,3 +1,4 @@
-export const listTasks = document.querySelector(".list");
 export const btnAddTask = document.querySelector(".btn--add-task");
+export const inputAddTask = document.querySelector(".add-task");
+export const listTasks = document.querySelector(".list");
 export const timeCurrentDate = document.querySelector(".current-date");
--- dist/types/domElements.d.ts
+++ dist/types/domElements.d.ts
@@ -1,3 +1,4 @@
-export declare const listTasks: Element | null;
 export declare const btnAddTask: Element | null;
+export declare const inputAddTask: Element | null;
+export declare const listTasks: Element | null;
 export declare const timeCurrentDate: Element | null;
